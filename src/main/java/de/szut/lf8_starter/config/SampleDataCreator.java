a5671c1814dcfcda66b0b601d976ce00c55fed0f
Saad Rafia <sar@abatgroup.de>
2026-01-07 02:17:06 +0100

    Add sample workout program data to SampleDataCreator

--- src/main/java/de/szut/lf8_starter/config/SampleDataCreator.java
+++ src/main/java/de/szut/lf8_starter/config/SampleDataCreator.java
@@ -8,14 +8,19 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import de.szut.lf8_starter.workout.WorkoutProgramEntity;
+import de.szut.lf8_starter.workout.WorkoutProgramRepository;
 
 @Component
 public class SampleDataCreator implements ApplicationRunner {
 
     private HelloRepository repository;
+    private WorkoutProgramRepository workoutProgramRepository;
 
-    public SampleDataCreator(HelloRepository repository) {
+
+    public SampleDataCreator(HelloRepository repository, WorkoutProgramRepository workoutProgramRepository) {
         this.repository = repository;
+        this.workoutProgramRepository = workoutProgramRepository;
     }
 
     public void run(ApplicationArguments args) {
@@ -23,11 +28,18 @@ public class SampleDataCreator implements ApplicationRunner {
         repository.save(new HelloEntity("Schöner Tag heute"));
         repository.save(new HelloEntity("FooBar"));
 
+        workoutProgramRepository.save(new WorkoutProgramEntity(
+        "Starting Strength", "Full Body", "Beginner", 12, 3));
+        workoutProgramRepository.save(new WorkoutProgramEntity(
+        "PPL Hypertrophy", "Full Body", "Intermediate", 16, 6));
+        workoutProgramRepository.save(new WorkoutProgramEntity(
+        "German Volume Training", "Full Body", "Advanced", 6, 4));
     }
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
 
 }
